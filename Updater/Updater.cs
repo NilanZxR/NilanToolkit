3061d81859f82e8346e34f8fe94b6a57f3d17185
Nilan <>
2021-12-20 20:17:39 +0800

    update

--- Updater/Updater.cs
+++ Updater/Updater.cs
@@ -1,29 +1,51 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 namespace NilanToolkit.Updater {
     public class Updater : MonoBehaviour {
+        
+        private enum OperationType {
+            Add,
+            Remove
+        }
+
+        private struct Operation<T> {
+            public OperationType type;
+            public T target;
+        }
         
         private static Updater _instance;
         private List<IUpdate> _updates = new List<IUpdate>();
         private List<ILateUpdate> _lateUpdates = new List<ILateUpdate>();
         private List<IFixedUpdate> _fixedUpdates = new List<IFixedUpdate>();
 
+        private static Queue<Operation<IUpdate>> _updateOperation = new Queue<Operation<IUpdate>>();
+        private static Queue<Operation<ILateUpdate>> _lateUpdateOperation = new Queue<Operation<ILateUpdate>>();
+        private static Queue<Operation<IFixedUpdate>> _fixedUpdateOperation = new Queue<Operation<IFixedUpdate>>();
+
         public static void Register(object target) {
             if (target == null) return;
             EnsureInstance();
-            if (target is IUpdate update) {
-                _instance._updates.Add(update);
-            }
-
-            if (target is ILateUpdate lateUpdate) {
-                _instance._lateUpdates.Add(lateUpdate);
-            }
-
-            if (target is IFixedUpdate fixedUpdate) {
-                _instance._fixedUpdates.Add(fixedUpdate);
+            switch (target) {
+                case IUpdate update:
+                    _updateOperation.Enqueue(new Operation<IUpdate>() {
+                        type = OperationType.Add,
+                        target = update
+                    });
+                    break;
+                case ILateUpdate lateUpdate:
+                    _lateUpdateOperation.Enqueue(new Operation<ILateUpdate>() {
+                        type = OperationType.Add,
+                        target = lateUpdate
+                    });
+                    break;
+                case IFixedUpdate fixedUpdate:
+                    _fixedUpdateOperation.Enqueue(new Operation<IFixedUpdate>() {
+                        type = OperationType.Add,
+                        target = fixedUpdate
+                    });
+                    break;
             }
         }
 
@@ -31,16 +53,25 @@ namespace NilanToolkit.Updater {
             if (target == null) return;
             if (!_instance) return;
             
-            if (target is IUpdate update) {
-                _instance._updates.Remove(update);
-            }
-
-            if (target is ILateUpdate lateUpdate) {
-                _instance._lateUpdates.Remove(lateUpdate);
-            }
-
-            if (target is IFixedUpdate fixedUpdate) {
-                _instance._fixedUpdates.Remove(fixedUpdate);
+            switch (target) {
+                case IUpdate update:
+                    _updateOperation.Enqueue(new Operation<IUpdate>() {
+                        type = OperationType.Remove,
+                        target = update
+                    });
+                    break;
+                case ILateUpdate lateUpdate:
+                    _lateUpdateOperation.Enqueue(new Operation<ILateUpdate>() {
+                        type = OperationType.Remove,
+                        target = lateUpdate
+                    });
+                    break;
+                case IFixedUpdate fixedUpdate:
+                    _fixedUpdateOperation.Enqueue(new Operation<IFixedUpdate>() {
+                        type = OperationType.Remove,
+                        target = fixedUpdate
+                    });
+                    break;
             }
         }
 
@@ -61,17 +92,44 @@ namespace NilanToolkit.Updater {
         }
 
         private void Update() {
+            while (_updateOperation.Count > 0) {
+                var operation = _updateOperation.Dequeue();
+                switch (operation.type) {
+                    case OperationType.Add:
+                        _updates.Add(operation.target);
+                        break;
+                    case OperationType.Remove:
+                        _updates.Remove(operation.target);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            
             foreach (var update in _updates) {
                 try {
                     update.OnUpdate();
                 }
                 catch (Exception e) {
-                    Debug.Log(e);
+                    Debug.LogError(e);
                 }
             }
         }
 
         private void LateUpdate() {
+            while (_lateUpdateOperation.Count > 0) {
+                var operation = _lateUpdateOperation.Dequeue();
+                switch (operation.type) {
+                    case OperationType.Add:
+                        _lateUpdates.Add(operation.target);
+                        break;
+                    case OperationType.Remove:
+                        _lateUpdates.Remove(operation.target);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
             foreach (var lateUpdate in _lateUpdates) {
                 try {
                     lateUpdate.OnLateUpdate();
@@ -83,6 +141,19 @@ namespace NilanToolkit.Updater {
         }
 
         private void FixedUpdate() {
+            while (_fixedUpdateOperation.Count > 0) {
+                var operation = _fixedUpdateOperation.Dequeue();
+                switch (operation.type) {
+                    case OperationType.Add:
+                        _fixedUpdates.Add(operation.target);
+                        break;
+                    case OperationType.Remove:
+                        _fixedUpdates.Remove(operation.target);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
             foreach (var fixedUpdate in _fixedUpdates) {
                 try {
                     fixedUpdate.OnFixedUpdate();
